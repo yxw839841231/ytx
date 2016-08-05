dad29af28e599cd822f3767f0c8e11923f47dfbd
yangxiaowei <zhonyangxiaowei@sina.com>
2016-08-05 17:15:18 +0800

    初始化项目构型

---
+++ src/main/java/com/ytx/web/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2014~2016 浙江格家网络科技. All Rights Reserved.
+ */
+package com.ytx.web;
+
+/**
+ * @author: yangxw  Date: 2016/8/5 Time: 17:13
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
