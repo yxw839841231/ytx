ecd2cb2ce1b2c6f9e48dc73daa879ef9229a4e50
yangxiaowei <zhonyangxiaowei@sina.com>
2016-08-06 17:56:28 +0800

    初始化

---
+++ src/main/java/com/ytx/web/entity/User.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2014~2016 浙江格家网络科技. All Rights Reserved.
+ */
+package com.ytx.web.entity;
+
+/**
+ * @author: yangxw  Date: 2016/8/6 Time: 17:16
+ */
+public class User {
+
+}
